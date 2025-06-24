f81ffccf6c0ca2ad1ddc923bfb0d9a8514c96456
Luisflores8081 <lflore21@students.kennesaw.edu>
2025-06-24 17:45:24 -0400

    added changes to modules

--- 3 calcGUI/calcGUI.py
+++ 3 calcGUI/calcGUI.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from math import sqrt as sqr
-
+from math import sin
+from math import cos
+from math import tan
 
 class Calculator(Frame):
     """
@@ -144,6 +146,15 @@ class Calculator(Frame):
         Creates the widgets to be used in the grid.
         :return: None
         """
+        self.sin_bttn = Button(self, text="sin", width=9, height=3, command=lambda: self.add_chr('sin('))
+        self.sin_bttn.grid(row=1, column=6)
+
+        self.cos_bttn = Button(self, text="cos", width=9, height=3, command=lambda: self.add_chr('cos('))
+        self.cos_bttn.grid(row=2, column=6)
+
+        self.tan_bttn = Button(self, text="tan", width=9, height=3, command=lambda: self.add_chr('tan('))
+        self.tan_bttn.grid(row=3, column=6)
+        
         self.eq_bttn = Button(self, text="=", width=20, height=3, bg="lightgrey", command=lambda: self.calculate())
         self.eq_bttn.grid(row=4, column=4, columnspan=2)
 
@@ -151,7 +162,7 @@ class Calculator(Frame):
         self.ac_bttn.grid(row=1, column=4)
 
         self.c_bttn = Button(self, text='←', width=9, height=3, command=lambda: self.clear())
-        self.c_bttn.grid(row=1, column=5 )
+        self.c_bttn.grid(row=1, column=5)
 
         self.add_bttn = Button(self, text="+", width=9, height=3, command=lambda: self.add_chr('+'))
         self.add_bttn.grid(row=4, column=3)
@@ -186,7 +197,7 @@ class Calculator(Frame):
         self.six_bttn = Button(self, text="6", width=9, height=3, command=lambda: self.add_chr(6))
         self.six_bttn.grid(row=2, column=2)
 
-        self.one_bttn = Button(self, text="1", width=9, height=3, command=lambda: self.add_chr(1))
+        self.one_bttn = Button(self, text="luis", width=9, height=3, command=lambda: self.add_chr("flore"))
         self.one_bttn.grid(row=3, column=0)
 
         self.two_bttn = Button(self, text="2", width=9, height=3, command=lambda: self.add_chr(2))
